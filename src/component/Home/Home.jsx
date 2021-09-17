a2493c5981729bc2ef39632019354709f8dd73d7
rirakib <rirrakib10@gmail.com>
2021-09-17 10:13:18 -0700

    commit

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 import './home.css'
+import HomeData from './HomeData'
 
 const Home = () => {
     return (
         <>
             <div className="container">
-                <d
+                <div className="wrapper">
+                    <div className="left_side">
+                        <div className="img_box">
+                            <img src={HomeData[0].imgsrc} />
+                        </div>
+                    </div>
+                    <div className="right_side">
+                        <h1>{HomeData[0].name}</h1>
+                        <h5>{HomeData[0].profassion}</h5>
+                        <p>{HomeData[0].details}</p>
+
+                        <div className="hire_btn">
+                            <a href={HomeData[0].cvlink} target="_blank">Hire Me</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Home
---
+++ src/component/Home/HomeData.jsx
@@ -0,0 +1,11 @@
+const HomeData = [
+    {
+        name:"Md. Rakibul Islam",
+        profassion: "React & Laravel Developer",
+        details: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+        imgsrc: "https://media.istockphoto.com/photos/handsome-asian-man-working-in-the-creative-workplace-picture-id1219975277?b=1&k=20&m=1219975277&s=170667a&w=0&h=C9veYRsZ5wfCbfDaYWXC3OUD7QjOnz-UV78YZwSFfVc=",
+        cvlink:"https://github.com/rirakib",
+    }
+]
+
+export default HomeData;
